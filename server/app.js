ea273609cfae09aa134af5cc37c4f72ded00d629
Halo-One <asvicki2002@gmail.com>
2024-08-04 13:34:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -40,8 +40,8 @@ app.use(express.static(path.join(__dirname, "../public")));
 //  Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
   console.log("* route working", __dirname)
-  res.sendFile("Hello world");
-  // res.sendFile(path.join(__dirname, "../public/index.html"));
+  // res.sendFile("Hello world");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, () => {
