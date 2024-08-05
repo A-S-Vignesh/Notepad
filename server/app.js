6675a885d4e62af3a237966127e2523c6e93dfa9
Halo-One <asvicki2002@gmail.com>
2024-08-05 22:50:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use("/api", apiRoutes);
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"client"));
+  res.sendFile(path.join(__dirname,"client","dist","index.html"));
 });
 
 app.listen(PORT, () => {
