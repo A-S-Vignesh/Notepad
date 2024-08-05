25a41c0729547a53cd71df3f4284c6fdc956354f
Halo-One <asvicki2002@gmail.com>
2024-08-05 22:04:36 +0530

    deployment

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use("/api", apiRoutes);
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,"client","index.html"));
+  res.sendFile(path.join(__dirname,"client"));
 });
 
 app.listen(PORT, () => {
