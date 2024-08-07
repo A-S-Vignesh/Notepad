ede67dd28b74e11e0792f680a6c0dce683cc5844
Halo-One <asvicki2002@gmail.com>
2024-08-07 18:31:36 +0530

    Added Scroll to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import "./index.css";
 import Header from "./components/Header";
 import Notes from "./components/Notes";
 import useFetchNotes from "./hooks/useFetchNotes";
+import ScrollToTopButton from "./components/ScrollToTop";
 
 const App = () => {
   useFetchNotes();
   return (
-    <div>
+    <div className="relative">
       <Header />
       <Notes />
+      <ScrollToTopButton />
     </div>
   );
 };
